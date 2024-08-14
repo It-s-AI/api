0ee8fc7a076a3492dee7a99975a3963c840f10e4
root <root@taomonitor.ip-ptr.tech>
2024-08-14 13:42:13 +0200

    new protocol for segmentation

--- protocol.py
+++ protocol.py
@@ -1,6 +1,24 @@
+# The MIT License (MIT)
+ # Copyright © 2024 It's AI 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 import pydantic
 from typing import List, Optional
 import bittensor as bt
+
+from detection import __version__
 
 
 class TextSynapse(bt.Synapse):
@@ -22,18 +40,13 @@ class TextSynapse(bt.Synapse):
         allow_mutation=False,
     )
 
-    predictions: List[float] = pydantic.Field(
+    predictions: List[List[float]] = pydantic.Field(
         ...,
         title="Predictions",
         description="List of predicted probabilities. This attribute is mutable and can be updated.",
     ) 
 
-    required_hash_fields: List[str] = pydantic.Field(
-        ["texts", "predictions"],
-        title="Required Hash Fields",
-        description="A list of required fields for the hash.",
-        allow_mutation=False,
-    )
+    version: str = ""
 
     def deserialize(self) -> float:
         """
